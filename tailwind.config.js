54e12a86bca8ee5583a4ec21d41caa45c4b8e2d9
hhyanghh <yanghh711@naver.com>
2024-04-02 11:16:45 +0900

    💄 set theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FFD0EC", // 핑크색, 주 포인트 색상
+        secondary: "#474F7A", // 남색, 보조 색상
+        accent: "#E14D2A", // 빨간색, 강조 색상
+        gray: "#D9D9D9", // 기본 회색
+      },
+    },
   },
   plugins: [],
 };
