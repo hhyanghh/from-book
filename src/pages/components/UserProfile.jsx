3970fe83b329fde94ac000d2d1acb1b019713e04
hhyanghh <yanghh711@naver.com>
2024-04-04 10:01:49 +0900

    ✅ UserProfile 드롭다운메뉴 추가

--- src/pages/components/UserProfile.jsx
+++ src/pages/components/UserProfile.jsx
@@ -1,16 +1,34 @@
 import React from "react";
+import { Menu } from "@headlessui/react";
+import { Link } from "react-router-dom";
+import Button from "./UI/Button";
 
 const UserProfile = ({ user }) => {
-  console.log(user, "userProfile>>>");
   return (
-    <div className="flex items-center">
-      <img
-        src={user.photoURL}
-        alt={user.displayName}
-        className="w-10 h-10 rounded-full"
-      />
-      <span className="ml-2 text-white">{user.displayName}</span>
-    </div>
+    <Menu>
+      <Menu.Button>
+        <div className="flex items-center">
+          <img
+            src={user.photoURL}
+            alt={user.displayName}
+            className="w-10 h-10 rounded-full"
+          />
+          <span className="ml-2 text-white">{user.displayName}</span>
+        </div>
+      </Menu.Button>
+      <Menu.Items>
+        <Menu.Item>
+          {({ active }) => (
+            <Link className={`${active && "bg-blue-500"}`} to="/myPage">
+              마이페이지
+            </Link>
+          )}
+        </Menu.Item>
+        <Menu.Item>
+          <Button text="로그아웃" />
+        </Menu.Item>
+      </Menu.Items>
+    </Menu>
   );
 };
 
