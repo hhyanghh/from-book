465a5d3455ecc7cca16d618f07a9b4f370e05e43
hhyanghh <yanghh711@naver.com>
2024-04-08 12:03:55 +0900

    💄 Home, QuoteCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import QuoteCard from "./components/QuoteCard";
 
-export default function Home() {
-  return <div>Home</div>;
-}
+const Home = () => {
+  const quote = "태양은 만인의 것, 바다는 즐기는 자의 것";
+  const source = "책, <종의 기원>";
+
+  return (
+    <div className="bg-secondary min-h-screen flex flex-col justify-center">
+      <QuoteCard quote={quote} source={source} />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/components/QuoteCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { FiHeart, FiPlusCircle } from "react-icons/fi";
+
+const QuoteCard = ({ quote, source }) => {
+  return (
+    <div className="max-w-xs mx-auto my-8 p-8 bg-white rounded-lg shadow-lg flex flex-col justify-between h-full">
+      <div className="text-center">
+        <p className="text-lg text-gray-800 font-semibold">{quote}</p>
+        <p className="text-sm text-gray-600 mt-2">{source}</p>
+      </div>
+      <div className="flex justify-end space-x-2 mt-4 text-gray-600">
+        <button type="button" className=" hover:text-primary">
+          <FiHeart size="24" />
+        </button>
+        <button type="button" className=" hover:text-primary">
+          <FiPlusCircle size="24" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default QuoteCard;
